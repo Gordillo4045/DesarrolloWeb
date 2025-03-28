3f37c3d9a868395f302f6cfacb1a0282a70b4166
Gordillo4045 <andre.vizuet14@gmail.com>
2025-03-28 15:45:09 -0700

    update Firebase hosting configuration and add deployment workflow; adjust layout styles in index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -93,7 +93,7 @@ export default function IndexPage() {
   return (
     <DefaultLayout>
       <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
-        <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[600px] w-[600px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]">
+        <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[600px] w-full md:w-[600px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]">
         </div>
       </div>
       <section className="flex flex-col items-center justify-center gap-4 py-8">
